b715d7a1af164b7a95ac03033c19664d94199422
arif1403 <arifmunandar.che@gmail.com>
2022-10-20 09:28:09 +0700

    initial deploy

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask , render_template, request
+# import pickle
+import joblib
+import numpy as np
+model = joblib.load("model/model1.pkl")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # return '<h1>Hello World</h1>'
+    return render_template("index.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data_features = dict(request.form).values()
+    data_features = np.array([float(x) for x in data_features])
+  
+    pred = model.predict([data_features])
+    return render_template('index.html', result=pred)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
